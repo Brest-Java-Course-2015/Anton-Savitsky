baaeffc72bf03d40ff1f115e001c72d4e9fc8051
antonsavitsky <anton.savitsky.1995@gmail.com>
2015-10-24 23:08:37 +0300

    delete, updateUser, getUserDto, findUserByLogin were added

--- app-rest/src/main/java/com/epam/brest/course2015/rest/UserRestController.java
+++ app-rest/src/main/java/com/epam/brest/course2015/rest/UserRestController.java
@@ -1,6 +1,7 @@
 package com.epam.brest.course2015.rest;
 
 import com.epam.brest.course2015.domain.User;
+import com.epam.brest.course2015.dto.UserDto;
 import com.epam.brest.course2015.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,17 +27,33 @@ public class UserRestController {
         return userService.getUserById(id);
     }
 
-    @RequestMapping(value = "/user/{login}/{password}", method = RequestMethod.POST)
-    public @ResponseBody Integer addUser(@PathVariable(value = "login") String login,
-                                         @PathVariable(value = "password") String password) {
-        return userService.addUser(new User(login, password));
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public @ResponseBody Integer addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void delete(@PathVariable(value = "id") Integer id){
+        userService.deleteUser(id);
     }
 
     @RequestMapping(value = "/user/{id}/{password}", method = RequestMethod.PUT)
-    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
     public void updateUser(@PathVariable(value = "id") Integer id,
                            @PathVariable(value = "password") String password) {
         userService.updateUser(new User(id, password));
     }
 
+    @RequestMapping(value = "/userdto", method = RequestMethod.GET)
+    public @ResponseBody
+    UserDto getUserDto(){
+        return userService.getUserDto();
+    }
+
+    @RequestMapping(value = "/user/{login}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.ACCEPTED)
+    public User findUserByLogin(@PathVariable(value = "login") String login) {
+        return  userService.findUserByLogin(login);
+    }
 }
